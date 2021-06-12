8e8cda14f55f97ae6259610d001f1312729ed797
nicholas-hoy-champain <nicholas.hoy@mymail.champlain.edu>
2021-06-11 20:57:48 -0400

    Replaced spelling error with new script for spawning placeholders with an offset

---
+++ Assets/Scripts/SpawnPlaceholdersInOrder.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlaceholdersInOrder : MonoBehaviour
+{
+    public GameObject placeHolder;
+    [SerializeField] int soliders;
+    [SerializeField] float size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetOffsets()
+    {
+        Vector2 offset = Vector2.zero;
+        int layer = 1, direction = 0, shift = 0,  layerFill = 0, layerSize = 8;
+
+        float xShift;
+
+        for(int i = 0; i < soliders; i++)
+        {
+            if(layerFill == layerSize)
+            {
+                layer++;
+                layerSize = 8 * layer;
+                layerFill = 0;
+                direction = 0; 
+                shift = 0;
+            }
+
+            if(direction == 4)
+            {
+                direction = 0;
+                shift++;
+            }
+
+            xShift = Mathf.Pow(-1, shift + 1) * Mathf.Floor(shift / 2.0f + .5f);
+
+            offset.x = xShift;
+            offset.y = layer;
+
+            if (direction == 1)
+                offset = new Vector2(offset.y, -offset.x);
+            else if (direction == 2)
+                offset = new Vector2(-offset.y, offset.x);
+            else if (direction == 3)
+                offset = -offset;
+
+            Instantiate(placeHolder, transform.position, Quaternion.identity);
+
+            direction++;
+        }
+    }
+}
