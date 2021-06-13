95215b09b3bde6769666e7baa6f68760e19e6257
Brandon Boras <brandon.boras@mymail.champlain.edu>
2021-06-13 00:18:59 -0400

    camera is now constrained to the level bg

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,10 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject target;
     [SerializeField] [Range(0.01f, 0.99f)] float cameraMovespeed;
+    
+    Vector2 BottomLeftBoundary = new Vector2(-2.97f, -5.2f);
+    Vector2 TopRightBoundary   = new Vector2( 2.97f, 5.95f) ;
+
 
     const float CAMERA_Z_POSITION = -10.0f;
 
@@ -19,6 +23,18 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         Vector2 newDistance = Vector2.Lerp(transform.position, target.transform.position, cameraMovespeed);
+
+        if (newDistance.x < BottomLeftBoundary.x)
+            newDistance = new Vector2(BottomLeftBoundary.x, newDistance.y);
+        if (newDistance.y < BottomLeftBoundary.y)
+            newDistance = new Vector2(newDistance.x, BottomLeftBoundary.y);
+
+
+        if (newDistance.x > TopRightBoundary.x)
+            newDistance = new Vector2(TopRightBoundary.x  , newDistance.y);
+        if (newDistance.y > TopRightBoundary.y)
+            newDistance = new Vector2(newDistance.x  , TopRightBoundary.y);
+
         transform.position = new Vector3(newDistance.x, newDistance.y, CAMERA_Z_POSITION); 
     }
 }
